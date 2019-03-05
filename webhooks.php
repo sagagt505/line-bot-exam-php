4822dd7d936769d21094224705c7644cbd6f343b
sagagt505 <sagagt505@users.noreply.github.com>
2019-03-05 16:38:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'wIaqzqXgwiMjN+awXH1DUd1nw/oy+O0viRI3LpCWqFvQhKI5DIicHl7h9Ldr4ImXywSU+z+658ApqbVK428VYQmahIWnhMJGtR+bHJOCyrjl1gJl16aeBNxZuKqD5BFa0efJ0Gwhq4uteWxdyUqrCwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
